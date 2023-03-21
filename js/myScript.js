e9e243ec54a2a5d81710f73c000683eb4c9819d2
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-03-21 16:20:36 -0500

    changing button text on every state

--- js/myScript.js
+++ js/myScript.js
@@ -6,12 +6,28 @@ const footer = document.querySelector(".footer");
 // EVENTOS ----------------------------
 
 botonFooter.addEventListener('click', aparecer);
-
+botonFooter.addEventListener('click', comprobarEstado);
 
 // FUNCTIONS ------------------------------
 
 function aparecer() {
     console.log("apareciendo");
     footer.classList.toggle("aparecer");  
-      
 }
+
+function comprobarEstado() {
+    
+    if (footer.classList.contains('aparecer')) {
+        console.log("El footer esta activo");
+        botonFooter.textContent = "Ocultar Footer"
+        botonFooter.style.backgroundColor = "red";
+        botonFooter.style.color = "white";
+       
+    } else {
+        console.log("No hay Hover");
+        botonFooter.textContent = "Aparecer Footer"
+        botonFooter.style.backgroundColor = "greenyellow";
+        botonFooter.style.color = "black";
+    }
+}
+
